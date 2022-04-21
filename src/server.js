aea1738b764c023eae54aa1e3ea848875cc3ec4e
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-21 16:36:21 -0300

    Rocketing!

--- src/server.js
+++ src/server.js
@@ -1,2 +1,8 @@
-const express = require("express");
+const express = require('express');
+const server = express();
 
+server.get('/', (request, response) => {
+  return response.send('OIE!!');
+});
+
+server.listen(3000, () => console.log('rodando!'));
