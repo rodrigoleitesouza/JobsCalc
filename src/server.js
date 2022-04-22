efeb59ca14890e61c39f605af167d3f7c8ce1819
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-22 18:07:04 -0300

    Routing!

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const server = express();
+const routes = require('./routes');
 
-server.get('/', (request, response) => {
-  return response.send('OIE22222!!');
-});
+server.use(express.static("public"))
+
+server.use(routes);
 
 server.listen(3000, () => console.log('rodando9999999'));
