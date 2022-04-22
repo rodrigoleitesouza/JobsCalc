2f96b533f0d73c9de24659edcc17751b56f16b4b
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-22 19:38:32 -0300

    Rocketing forever!

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const routes = express.Router();
 
-const basePath = __dirname + "/views";
+const views = __dirname + "/views/";
 
-routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"));
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"));
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"));
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"));
+const profile = {
+  name: "Rodrigo",
+  avatar: "https://avatars.githubusercontent.com/u/96425377?v=4",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4,
+}
+
+routes.get('/', (req, res) => res.render(views + "index"));
+routes.get('/job', (req, res) => res.render(views + "job"));
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const server = express();
 const routes = require('./routes');
 
-server.use(express.static("public"))
+server.set("view engine", "ejs");
+
+server.use(express.static("public"));
 
 server.use(routes);
 
-server.listen(3000, () => console.log('rodando9999999'));
+server.listen(3000);
