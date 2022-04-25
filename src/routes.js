674f6da2c2a8e86ed5a7904f680b49ebd98be972
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-25 17:08:25 -0300

    Come n' get it!

--- src/routes.js
+++ src/routes.js
@@ -12,14 +12,36 @@ const profile = {
   "vacation-per-year": 4,
 };
 
-const jobs = [];
+const jobs = [
+  {
+    id: 1,
+    name: "Rodrigo",
+    "daily-hours": 2,
+    "total-hours": 16,
+    created_at: Date.now(),
+  },
+  {
+    id: 2,
+    name: "Milena",
+    "daily-hours": 99,
+    "total-hours": 999,
+    created_at: Date.now(),
+  },
+];
 
-routes.get('/', (req, res) => res.render(views + "index"));
+routes.get('/', (req, res) => res.render(views + "index", { jobs }));
 routes.get('/job', (req, res) => res.render(views + "job"));
 
 routes.post('/job', (req, res) => {
-  jobs.push(req.body)
-  console.log(jobs)
+  const lastId = jobs[jobs.length - 1]?.id || 1;
+
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    created_at: Date.now(),
+  })
   return res.redirect('/')
 });
 
