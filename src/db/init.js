9813c4a9fb1e98f3b3bb27c60ba944a793d82743
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-05-02 20:06:09 -0300

    Oh my gozh!

--- src/db/init.js
+++ src/db/init.js
@@ -1,76 +1,83 @@
-const Database = require("config");
+const Database = require("./config");
 
-Database();
+const initDB = {
 
-Database.exec(`
-  CREATE TABLE profile (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly_budget INT,
-    days_per_week INT,
-    hours_per_day INT,
-    vacation_per_year INT,
-    value_hour INT,
-  )
-`);
+  async init() {
+    const db = await Database();
 
-Database.exec(`
-  CREATE TABLE jobs (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    daily_hours INT,
-    total_hours INT,
-    created_at DATETIME,
-  )
-`);
+    await db.exec(`
+      CREATE TABLE profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        monthly_budget INT,
+        days_per_week INT,
+        hours_per_day INT,
+        vacation_per_year INT,
+        value_hour INT
+      )
+    `);
 
-Database.run(`
-  INSERT INTO profile (
-    name,
-    avatar,
-    monthly_budget,
-    days_per_week,
-    hours_per_day,
-    vacation_per_year,
-    value_hour,
-  ) VALUES (
-    "Rodrigo Souza",
-    "https://github.com/rodrigoleitesouza.png",
-    3000,
-    5,
-    5,
-    4,
-    75,
-  ); 
-`);
+    await db.exec(`
+      CREATE TABLE jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME
+      )
+    `);
 
-Database.run(`
-  INSERT INTO jobs (
-    name,
-    daily_hours,
-    total_hours,
-    created_at,
-  ) VALUES (
-    "Pizzaria Guloso 999",
-    2,
-    1,
-    1617514376018,
-  ); 
-`);
+    await db.run(`
+      INSERT INTO profile (
+        name,
+        avatar,
+        monthly_budget,
+        days_per_week,
+        hours_per_day,
+        vacation_per_year,
+        value_hour
+      ) VALUES (
+        "Rodrigo Souza 999",
+        "https://github.com/rodrigoleitesouza.png",
+        3000,
+        5,
+        5,
+        4,
+        75
+      ); 
+    `);
 
-Database.run(`
-  INSERT INTO jobs (
-    name,
-    daily_hours,
-    total_hours,
-    created_at,
-  ) VALUES (
-    "OneTwo Project 999",
-    3,
-    47,
-    1617514376028,
-  ); 
-`);
+    await db.run(`
+      INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+      ) VALUES (
+        "Pizzaria Guloso 999",
+        2,
+        1,
+        1617514376018
+      ); 
+    `);
 
-Database.close();
+    await db.run(`
+      INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+      ) VALUES (
+        "OneTwo Project 999",
+        3,
+        47,
+        1617514376028
+      ); 
+    `);
+
+    await db.close();
+  },
+};
+
+initDB.init();
