5d58d36777ea5a59d38f9cd17253a462808b36d3
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-05-02 19:25:30 -0300

    Initing!

--- src/db/init.js
+++ src/db/init.js
@@ -7,12 +7,70 @@ Database.exec(`
     id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     avatar TEXT,
-    monthly-budget INT,
-    days-per-week INT,
-    hours-per-day INT,
-    vacation-per-year INT,
-    value-hour INT,
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT,
   )
+`);
+
+Database.exec(`
+  CREATE TABLE jobs (
+    id INT PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME,
+  )
+`);
+
+Database.run(`
+  INSERT INTO profile (
+    name,
+    avatar,
+    monthly_budget,
+    days_per_week,
+    hours_per_day,
+    vacation_per_year,
+    value_hour,
+  ) VALUES (
+    "Rodrigo Souza",
+    "https://github.com/rodrigoleitesouza.png",
+    3000,
+    5,
+    5,
+    4,
+    75,
+  ); 
+`);
+
+Database.run(`
+  INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at,
+  ) VALUES (
+    "Pizzaria Guloso 999",
+    2,
+    1,
+    1617514376018,
+  ); 
+`);
+
+Database.run(`
+  INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at,
+  ) VALUES (
+    "OneTwo Project 999",
+    3,
+    47,
+    1617514376028,
+  ); 
 `);
 
 Database.close();
